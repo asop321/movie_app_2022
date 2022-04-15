1b74c786ed90813304e859ab2111c08e3e550fb9
asop321 <asop321@gmail.com>
2022-04-15 11:50:15 +0900

    05 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 import React from 'react';
 
-class App extends React.Component{
-  constructor(props){
-    super(props)
-    console.log('constructor 실행');
-  }
-
-  state = {count:0};
-  add = () => {
-    this.setState({count: this.state.count + 1})
-  }
-  minus = () => {
-    this.setState({count: this.state.count - 1})
-  }
-  componentDidMount(){
-    console.log('componentDidMount 실완')
-  }
-  componentDidUpdate(){
-    console.log('componentDidUpdate 실완')
-  }
-  componentWillUnmount(){
-    console.log("비번뭐야");
-  }
-  render(){
-    console.log('render 실완')
-    return (
-      <div>
-        <h1>번호는{this.state.count}</h1>
-        <button onClick = {this.add}>더하기</button>
-        <button onClick = {this.minus}>빼기</button>
-      </div>)
-  }
+class App extends React.Component {
+state = {
+        isLoading : true,
+        movies:[]
+    }
+    componentDidMount(){
+      setTimeout(()=>{
+        this.setState({
+          isLoading:false,})
+        },5000)
+      }
+    
+    render() {
+      const {isLoading}=this.state;
+        return (
+          <div>
+            {isLoading ? '로딩중...':'로딩완료'}
+          </div>
+        )
+    }
 }
-
 
 export default App;
