9decbfbf39030a03f77b70c4e4bcc6c4bcd810dd
asop321 <asop321@gmail.com>
2022-04-19 11:38:43 +0900

    06 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
-  }
+  };
   getMovies = async () => {
     const {
       data: {
         data: { movies },
       },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json')
-    this.setState({movies,isLoading:false})
-  }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  };
+
   componentDidMount() {
-    this.getMovies()
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state
-    return <div>{isLoading ? '로딩중...' : '로딩완료'}</div>
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        <section class="container">
+          {isLoading ? (
+            <div class="loader">
+              <span class="loader__text">"로딩중..."</span>
+            </div>
+          ) : (
+            <div class="movies">
+              {movies.map(movie => (
+                <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+              ))}
+            </div>
+          )}
+        </section>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
