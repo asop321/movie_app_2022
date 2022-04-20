f43504846b3f0710a37b8ab4919392a1365be8a7
asop321 <asop321@gmail.com>
2022-04-20 11:08:43 +0900

    07 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -33,7 +33,14 @@ class App extends React.Component {
           ) : (
             <div class="movies">
               {movies.map(movie => (
-                <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+                <Movie 
+                key={movie.id} 
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} poster={movie.medium_cover_image} 
+                genres={movie.genres}
+                />
               ))}
             </div>
           )}
--- src/movie.js
+++ src/movie.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import "./Movie.css";
+import propTypes from 'prop-types';
 
-function Movie({  title, year, summary, poster }) {
+function Movie({  title, year, summary, poster ,genres }) {
     return (
-<div class="movie">
+<div class="movie__data">
             <img src={poster} alt={title} title={title} />
             <div class="movie__data">
                 <h3 class="movie__title" >{title}</h3>
                 <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
+                <ul class='movie__genres'>
+                    {genres.map((genre,index)=>{
+                        return <li key={index}class='movie_genre'>{genre}</li>
+                    })}
+                </ul>
+                <p class="movie__summary">{summary.slice(0,180)}...</p>
             </div>
 </div>
     );
@@ -21,6 +27,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres:propTypes.arrayOf(propTypes.string).isRequired
 };
 
 export default Movie;
